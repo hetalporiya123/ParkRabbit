19bbd899ed6655c879d731192608ba3f2baa0e94
1573 <52pohe1mst@hft-stuttgart.de>
2025-12-20 20:28:56 +0100

    Add backend Dockerfile and docker-compose configuration

--- backend/src/main/java/com/parkrabbit/backend/BackendApplication.java
+++ backend/src/main/java/com/parkrabbit/backend/BackendApplication.java
@@ -2,15 +2,11 @@ package com.parkrabbit.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-        dotenv.entries().forEach(e ->
-            System.setProperty(e.getKey(), e.getValue())
-        );
 
 		SpringApplication.run(BackendApplication.class, args);
 	}
